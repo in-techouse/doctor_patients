987fa427b8f16d0ccca0726d850637454bcf250a
In-TecHouse <team.dev.man@gmail.com>
2020-12-31 17:49:57 +0500

    iOS build generated

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,6 @@
 import 'package:doctor_app/view/Splash_Screen.dart';
-import 'package:doctor_app/view/auth/Login_Screen.dart';
-import 'package:doctor_app/view/patient_ui/Patient_DashBoard.dart';
-import 'package:doctor_app/view/patient_ui/diseases.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-
-import 'doctor_ui/doctorChat.dart';
-import 'model/constants.dart';
-import 'model/users.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,12 +9,11 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Doctor Patients',
+      title: 'HelloDoc24',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -31,5 +21,4 @@ class MyApp extends StatelessWidget {
       home: SplashScren(),
     );
   }
-
 }
